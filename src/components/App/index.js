c14caa6f4b8303e884c7b506682610adaeb03cc7
Deivid <deivid.cardos@gmail.com>
2022-05-30 20:12:52 -0300

    add routes into project

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,24 +1,16 @@
-import { ThemeProvider } from 'styled-components';
-
-import GlobalStyles from '../../assets/styles/global';
-import defaultTheme from '../../assets/styles/themes/default';
-
 function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyles />
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          width: '100vw',
-          height: '100vh',
-        }}
-      >
-        <span style={{ fontSize: '48px' }}>🚧 Work in progress</span>
-      </div>
-    </ThemeProvider>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100vw',
+        height: '100vh',
+      }}
+    >
+      <span style={{ fontSize: '48px' }}>🚧 Work in progress</span>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+
 import App from './components/App';
+
+import GlobalStyles from './assets/styles/global';
+import defaultTheme from './assets/styles/themes/default';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
 );
